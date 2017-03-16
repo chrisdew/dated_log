9bc6735b3b66c9ca2f144d6ed5d9bf20884d8ef1
chrisdew <chris.dew@thorcom.co.uk>
2017-03-16 12:27:53 +0000

    0.0.4

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dated_log
 Log to a dated, per-day, logfile.
+
+    var dated_log = require('dated_log');
+    var log = dated_log('/var/log/foo');
+    log({ts: new Date().toISOFormat(), pid: process.pid, bar: { baz: 'quux' }});
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var fs = require('fs');
 
+/*
 function Logger(prefix) {
   this._stream = null;
   this._date = null;
@@ -21,5 +22,30 @@ Logger.prototype.write = function(line) {
   }
   this._stream.write(line + '\n');
 };
+*/
 
-exports.Logger = Logger;
+function dated_log(prefix) {
+  var _stream = null;
+  var _date = null;
+  var _prefix = prefix;
+		
+	return function logger(ob) {
+    var today = new Date().toISOString().substring(0, 10);
+    if (today != _date) {
+      if (_stream) {
+        _stream.close();
+      }
+      _date = today;
+      _stream = fs.createWriteStream(_prefix + '.' + _date, {flags: 'a'});
+    }
+		if (typeof ob === 'string') {
+      _stream.write(ob + '\n');
+		} else if (ob.isBuffer && ob.isBuffer()) {
+      _stream.write(ob);
+		} else {
+      _stream.write(JSON.stringify(ob) + '\n');
+		}
+	}
+}
+
+module.exports = dated_log;
